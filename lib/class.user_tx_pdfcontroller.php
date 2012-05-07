0e78e0bf9dbe17df15ea67458037edc54cc91ad0
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-05-07 14:31:12 +0000

    1.2.2 +++ rawurlencode addQueryString
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/pdfcontroller/trunk@61629 735d13b6-9817-0410-8766-e36946ffe9aa

--- lib/class.user_tx_pdfcontroller.php
+++ lib/class.user_tx_pdfcontroller.php
@@ -74,7 +74,8 @@ class user_tx_pdfcontroller
 
 var_dump( __METHOD__, __LINE__, $content, $conf );
 
-    return '<a href="http.//die-netzmacher.de/">Die Netzmacher</a>';
+    return 'http://die-netzmacher.de/';
+    return '<a href="http://die-netzmacher.de/">Die Netzmacher</a>';
     return $content;
   }
 
