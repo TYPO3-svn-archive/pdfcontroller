1805c67e868a7ca6379145d11d8f8202531f6e5d
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-06-20 10:47:10 +0000

    TCPDF
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/pdfcontroller/trunk@89264 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/pi1/pdf/_setup.ts
+++
@@ -1,3 +0,0 @@
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:pdfcontroller/Configuration/TypoScript/pi1/pdf/filename.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:pdfcontroller/Configuration/TypoScript/pi1/pdf/footer.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:pdfcontroller/Configuration/TypoScript/pi1/pdf/properties.ts">
