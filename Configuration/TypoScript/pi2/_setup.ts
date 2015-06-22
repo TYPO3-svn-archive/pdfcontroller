add210062310fc5806c019b8d002af902d35fb16
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-06-22 21:06:41 +0000

    #i0007
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/pdfcontroller/trunk@89290 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/pi2/_setup.ts
+++ Configuration/TypoScript/pi2/_setup.ts
@@ -1,3 +1,2 @@
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:pdfcontroller/Configuration/TypoScript/pi2/button/setup.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:pdfcontroller/Configuration/TypoScript/pi2/master_templates/_setup.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:pdfcontroller/Configuration/TypoScript/pi2/_CSS_DEFAULT_STYLE/setup.ts">
--- Configuration/TypoScript/pi2/master_templates/_setup.ts
+++
@@ -1,5 +0,0 @@
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:pdfcontroller/Configuration/TypoScript/pi2/master_templates/pdf_and_print_button.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:pdfcontroller/Configuration/TypoScript/pi2/master_templates/pdf_and_print_button_rawUrlEncode.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:pdfcontroller/Configuration/TypoScript/pi2/master_templates/pdf_and_print_button_realUrl.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:pdfcontroller/Configuration/TypoScript/pi2/master_templates/pdf_link_rawUrlEncode.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:pdfcontroller/Configuration/TypoScript/pi2/master_templates/pdf_link_realUrl.ts">
