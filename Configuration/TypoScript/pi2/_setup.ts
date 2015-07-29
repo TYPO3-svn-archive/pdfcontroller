7ee6321522e9aacd1d6ccfeb45294218a6e66988
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-07-29 18:50:02 +0000

    4.0.0 +++ #i0009, #i0010: INCOMPLETE! PDF Controller doesn't work!
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/pdfcontroller/trunk@89508 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/pi2/_setup.ts
+++ Configuration/TypoScript/pi2/_setup.ts
@@ -1,2 +1,2 @@
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:pdfcontroller/Configuration/TypoScript/pi2/button/setup.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:pdfcontroller/Configuration/TypoScript/pi2/_CSS_DEFAULT_STYLE/setup.ts">
+//<INCLUDE_TYPOSCRIPT: source="FILE:EXT:pdfcontroller/Configuration/TypoScript/pi2/_CSS_DEFAULT_STYLE/setup.ts">
