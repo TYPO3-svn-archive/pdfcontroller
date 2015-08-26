b08f0043c78ab505cc6e21580a74182961a9c3b6
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-08-26 23:34:05 +0000

    4.0.6 +++ fe.lockIP
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/pdfcontroller/trunk@89620 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ Classes/Controller/testgoogle.php
@@ -0,0 +1,6 @@
+<?php
+
+//$fcont = file_get_contents('http://www.google.com/search?hl=fr&q=bpatterson');
+$fcont = file_get_contents('http://die-netzmacher.de/index.php?id=3685&type=67426&FE_SESSION_KEY=928d875528a5f26a5eb497ea0b9d3f28-376dadbedde679e7028e1387a4e09733');
+echo  $fcont;
+  
