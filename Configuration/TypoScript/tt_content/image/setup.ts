6946fc2c5e9eccfe4e9e2f772d9cc3767ec11ba1
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-08-29 12:05:23 +0000

    4.1.0 +++ Support for Start
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/pdfcontroller/trunk@89652 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/tt_content/image/setup.ts
+++ Configuration/TypoScript/tt_content/image/setup.ts
@@ -1,66 +1,68 @@
-## images
-tt_content.image {
-  20 {
-    renderMethod = simple
-    rendering {
-      simple {
-        imageStdWrapNoWidth.wrap = |
-        imageStdWrap.dataWrap = |
-        caption.wrap = |
+[globalVar = GP:type = {$plugin.pdfcontroller.pages.print.typeNum}]
+  ## images
+  tt_content.image {
+    20 {
+      renderMethod = simple
+      rendering {
+        simple {
+          imageStdWrapNoWidth.wrap = |
+          imageStdWrap.dataWrap = |
+          caption.wrap = |
+        }
       }
-    }
-    layout {
-      default {
-        value (
-          <table cellspacing="0" cellpadding="0" style="width: 100%; text-align: left; margin: 0;">
-            <tr>
-              <td valign="top" align="center" style="text-align: center;">
-                ###IMAGES###
-              </td>
-            </tr>
-            <tr>
-              <td valign="top">
-                ###TEXT###
-              </td>
-            </tr>
-          </table>
+      layout {
+        default {
+          value (
+            <table cellspacing="0" cellpadding="0" style="width: 100%; text-align: left; margin: 0;">
+              <tr>
+                <td valign="top" align="center" style="text-align: center;">
+                  ###IMAGES###
+                </td>
+              </tr>
+              <tr>
+                <td valign="top">
+                  ###TEXT###
+                </td>
+              </tr>
+            </table>
 )
-        override >
-      }
-        // 150622, dwildt, +: intext-right-nowrap
-      25 = TEXT
-      25 {
-        value (
-          <table cellspacing="0" cellpadding="0" style="width: 100%; text-align: left; margin: 0;">
-            <tr>
-              <td valign="top">
-                ###TEXT###
-              </td>
-              <td valign="top"style="padding:0 10px 0 10px;">
-                ###IMAGES###
-              </td>
-            </tr>
-          </table>
+          override >
+        }
+          // 150622, dwildt, +: intext-right-nowrap
+        25 = TEXT
+        25 {
+          value (
+            <table cellspacing="0" cellpadding="0" style="width: 100%; text-align: left; margin: 0;">
+              <tr>
+                <td valign="top">
+                  ###TEXT###
+                </td>
+                <td valign="top"style="padding:0 10px 0 10px;">
+                  ###IMAGES###
+                </td>
+              </tr>
+            </table>
 )
-        override >
-      }
-        // 150622, dwildt, +: intext-left-nowrap
-      26 = TEXT
-      26 {
-        value (
-          <table cellspacing="0" cellpadding="0" style="width: 100%; text-align: left; margin: 0;">
-            <tr>
-              <td valign="top"style="padding:0 10px 0 10px;">
-                ###IMAGES###
-              </td>
-              <td valign="top">
-                ###TEXT###
-              </td>
-            </tr>
-          </table>
+          override >
+        }
+          // 150622, dwildt, +: intext-left-nowrap
+        26 = TEXT
+        26 {
+          value (
+            <table cellspacing="0" cellpadding="0" style="width: 100%; text-align: left; margin: 0;">
+              <tr>
+                <td valign="top"style="padding:0 10px 0 10px;">
+                  ###IMAGES###
+                </td>
+                <td valign="top">
+                  ###TEXT###
+                </td>
+              </tr>
+            </table>
 )
-        override >
+          override >
+        }
       }
     }
   }
-}
+[global]
