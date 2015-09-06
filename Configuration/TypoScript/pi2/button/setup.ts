d2008d010690a11c82cb400df78da8d719bba7db
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-09-06 21:16:16 +0000

    4.1.1 +++ PDF Controller Button TEXT
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/pdfcontroller/trunk@89728 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/pi2/button/setup.ts
+++ Configuration/TypoScript/pi2/button/setup.ts
@@ -65,4 +65,57 @@ plugin.tx_pdfcontroller_pi2 {
       }
     }
   }
+    // empty statement for proper comments only
+  text {
+  }
+  text = TEXT
+  text {
+    value = PDF Download
+    lang {
+      de = PDF Download
+      en = PDF Download
+    }
+    typolink {
+      parameter {
+        cObject = COA
+        cObject {
+            // url
+          10 = TEXT
+          10 {
+            data = page:uid
+          }
+            // target
+          20 = TEXT
+          20 {
+            value       = _blank
+            noTrimWrap  = | "|"|
+          }
+            // class
+          30 = TEXT
+          30 {
+            value       = -
+            noTrimWrap  = | "|"|
+          }
+            // title
+          40 = TEXT
+          40 {
+            value       = -
+            stdWrap {
+              stripHtml         = 1
+              htmlSpecialChars  = 1
+              crop              = 30
+            }
+            noTrimWrap  = | "|"|
+          }
+        }
+      }
+      additionalParams  = &type={$plugin.pdfcontroller.pages.pdf.typeNum}
+        // #i0024, 150904, dwildt, 4+
+      addQueryString    = 1
+      addQueryString {
+        exclude = id, cHash
+      }
+      ATagParams        = rel="nofollow"
+    }
+  }
 }
