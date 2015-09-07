fd67d121a193b5954b3c9f19ae29a1587d452a58
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-09-07 16:15:15 +0000

    4.1.1 +++ Improve description, new icon
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/pdfcontroller/trunk@89736 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,40 +10,47 @@
 # writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
-$EM_CONF[$_EXTKEY] = array(
-	'title' => 'PDF Controller 3',
-	'description' => 'Offer your HTML page for PDF download. Controll the conversion with the user interface.',
-	'category' => 'plugin',
-	'shy' => 0,
-	'version' => '4.1.1',
-	'dependencies' => '',
-	'conflicts' => '',
-	'priority' => '',
-	'loadOrder' => '',
-	'module' => '',
-	'state' => 'beta',
-	'uploadfolder' => 1,
-	'createDirs' => '',
-	'modify_tables' => '',
-	'clearcacheonload' => 0,
-	'lockType' => '',
-	'author' => 'Dirk Wildt (Die Netzmacher)',
-	'author_email' => 'http://wildt.at.die-netzmacher.de',
-	'author_company' => '',
-	'CGLcompliance' => '',
-	'CGLcompliance_note' => '',
-	'constraints' => array(
-		'depends' => array(
-			'typo3' => '6.0.0-6.2.99',
-			't3_tcpdf' => '2.4.0-',
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-			'pdfcontroller_fonts' => '',
-		),
-	),
-	'_md5_values_when_last_written' => '',
+$EM_CONF[ $_EXTKEY ] = array(
+  'title' => 'PDF Controller 3',
+  'description' => 'Responsive TYPO3 to PDF: Offer your HTML page for PDF download. '
+  . 'Individual layout with background PDF file. '
+  . 'Extra first page is possible. '
+  . 'PDF Controller supports TYPO3 sessions and can used on restricted pages. '
+  . 'The PDF engine is TCPDF. '
+  . 'The PDF Controller 3 supports the TYPO3 responsive Start Kit (Start). '
+  . 'Start is based on Zurb Foundation. '
+  . 'With Start the PDF Contoller is able to handle responsive TYPO3 including grid layouts. '
+  ,
+  'category' => 'plugin',
+  'shy' => 0,
+  'version' => '4.1.1',
+  'dependencies' => '',
+  'conflicts' => '',
+  'priority' => '',
+  'loadOrder' => '',
+  'module' => '',
+  'state' => 'beta',
+  'uploadfolder' => 1,
+  'createDirs' => '',
+  'modify_tables' => '',
+  'clearcacheonload' => 0,
+  'lockType' => '',
+  'author' => 'Dirk Wildt (Die Netzmacher)',
+  'author_email' => 'http://wildt.at.die-netzmacher.de',
+  'author_company' => '',
+  'CGLcompliance' => '',
+  'CGLcompliance_note' => '',
+  'constraints' => array(
+    'depends' => array(
+      'typo3' => '6.0.0-6.2.99',
+      't3_tcpdf' => '2.4.0-',
+    ),
+    'conflicts' => array(
+    ),
+    'suggests' => array(
+      'pdfcontroller_fonts' => '',
+    ),
+  ),
+  '_md5_values_when_last_written' => '',
 );
-
 ?>
