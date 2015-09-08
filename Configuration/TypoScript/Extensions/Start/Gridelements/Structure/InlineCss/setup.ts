3673cb4a99e4ea0e5050090f77d231e7722b4376
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-09-08 21:02:07 +0000

    4.1.3 +++ tt_content.image caption got a p-wrap
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/pdfcontroller/trunk@89752 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/Extensions/Start/Gridelements/Structure/InlineCss/setup.ts
+++ Configuration/TypoScript/Extensions/Start/Gridelements/Structure/InlineCss/setup.ts
@@ -19,37 +19,25 @@ start {
       pdfcontroller {
         value (
 <style>
-  table.columns01 tr td {
-    width:100%;
-  }
+  table.columns01 { width:100%; }
+  table.columns02 { width:100%; }
+  table.columns03 { width:100%; }
+  table.columns04 { width:100%; }
+  table.columns05 { width:100%; }
+  table.columns06 { width:100%; }
+
+  table.columns01 tr td { width:100%; }
+  table.columns02 tr td { width:49%; }
+  table.columns03 tr td { width:32%; }
+  table.columns04 tr td { width:23.5%; }
+  table.columns05 tr td { width:18.4%; }
+  table.columns06 tr td { width:15%; }
+
   table.columns02 tr td.space { width:2%; }
   table.columns03 tr td.space { width:2%; }
   table.columns04 tr td.space { width:2%; }
   table.columns05 tr td.space { width:2%; }
   table.columns06 tr td.space { width:2%; }
-  table.columns02 tr td {
-    width:49%;
-  }
-  table.columns03 tr td {
-    width:32%;
-  }
-/*  table.columns03 tr td.first,
-  table.columns03 tr td.last {
-    width:32%;
-  }*/
-  table.columns04 tr td {
-    width:23.5%;
-  }
-  table.columns05 tr td {
-    width:18.4%;
-  }
-  table.columns06 tr td {
-    width:15%;
-  }
-/*  table.columns06 tr td.first,
-  table.columns06 tr td.last {
-    width:15%;
-  }*/
 </style>
 )
       }
